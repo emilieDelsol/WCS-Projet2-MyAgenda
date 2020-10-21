5cc64900894674ebdb69195007b89e46a1dc536d
emilieDelsol <emiliedelsol1@gmail.com>
2020-10-21 09:40:01 +0200

    comment httpPost

--- projet2Appointment/projet2Appointment/Appointment.cs
+++ projet2Appointment/projet2Appointment/Appointment.cs
@@ -16,6 +16,10 @@ namespace projet2Appointment
         public DateTime EndDate { get; set; }
 
         public string Description { get; set; }
-       
+
+        public string InsertAppointment { get; set; }
+        
+
     }
+   
 }
--- projet2Appointment/projet2Appointment/Controllers/AppointmentController.cs
+++ projet2Appointment/projet2Appointment/Controllers/AppointmentController.cs
@@ -11,29 +11,46 @@ namespace projet2Appointment.Controllers
     [Route("[controller]")]
     public class AppointmentController : ControllerBase
     {
+        List<Appointment> myList = new List<Appointment>();
 
         [HttpGet]
         public List<Appointment> Get()
         {
-            return new List<Appointment>
+            
+
+
+            myList.Add(new Appointment
             {
-                new Appointment
-                {
-                    Id = 1,
-                    Rdv = "My first RDV",
-                    BeginDate = new DateTime(2020, 8, 8, 16, 0,0),
-                    EndDate = new DateTime(2020,8,8,16,15,0),
-                    Description = "This is my first rdv",
-                },
-                new Appointment
-                {
-                    Id = 2,
-                    Rdv = "My second RDV",
-                    BeginDate = new DateTime(2020, 12, 8, 16, 0,0),
-                    EndDate = new DateTime(2020,12,8,16,15,0),
-                    Description="This is my second rdv :)",
-                }
-            };
+                Id = 1,
+                Rdv = "My first RDV",
+                BeginDate = new DateTime(2020, 8, 8, 16, 0, 0),
+                EndDate = new DateTime(2020, 8, 8, 16, 15, 0),
+                Description = "This is my first rdv",
+            });
+            myList.Add(new Appointment
+            {
+                Id = 2,
+                Rdv = "My second RDV",
+                BeginDate = new DateTime(2020, 12, 8, 16, 0, 0),
+                EndDate = new DateTime(2020, 12, 8, 16, 15, 0),
+                Description = "This is my second rdv :)",
+            });
+            
+            return myList;
         }
+
+        /*[HttpPost]
+        public string InsertAppointment(Appointment appointment)
+        {
+           Appointment myList. = new Appointment();
+            myAppointment.Id = appointment.Id;
+            myAppointment.Rdv = appointment.Rdv;
+            myAppointment.BeginDate = appointment.BeginDate;
+            myAppointment.EndDate = appointment.EndDate;
+            myAppointment.Description = appointment.Description;
+            return "congrats you add a new rdv";
+        }
+
+        }*/
     }
 }
