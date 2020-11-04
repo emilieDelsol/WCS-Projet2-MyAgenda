e5dcc380667f46cca639dea761294ecdc2172ddc
emilieDelsol <emiliedelsol1@gmail.com>
2020-11-04 12:15:31 +0100

    connect filter between date to BDD that's work ! :)

--- projet2Appointment/projet2Appointment/DataAbstractionLayer.cs
+++ projet2Appointment/projet2Appointment/DataAbstractionLayer.cs
@@ -83,6 +83,37 @@ namespace projet2Appointment
             reader.Close();
             return appointments;
         }
+        public static List<Appointment> GetBetweenDate(String beginDateFilter, String endDateFilter)
+        {
+            SqlCommand command = _connection.CreateCommand();
+            command.CommandText = "SELECT * FROM Appointment WHERE (BeginDate>=@beginDateFilter AND BeginDate<=@endDateFilter)";
+            command.Parameters.AddWithValue("@beginDateFilter", beginDateFilter);
+            command.Parameters.AddWithValue("@endDateFilter", endDateFilter);
+            SqlDataReader reader = command.ExecuteReader();
+            List<Appointment> appointments = new List<Appointment>();
+            while (reader.Read())
+            {
+                Appointment appointment = new Appointment
+                {
+                    Id = reader.GetInt32(0),
+                    Rdv = reader.GetString(1),
+                    BeginDate = reader.GetDateTime(2),
+                    EndDate = reader.GetDateTime(3),
+                    Description = reader.GetString(4),
+                    Address = reader.GetString(5),
+                    Contact = reader.GetString(6),
+                    Email = reader.GetString(7),
+                    Phone = reader.GetString(8),
+                    Importance = reader.GetInt32(9),
+                    Recurrence = reader.GetBoolean(10),
+                    Pro = reader.GetBoolean(12),
+                    Perso = reader.GetBoolean(13)
+                };
+                appointments.Add(appointment);
+            }
+            reader.Close();
+            return appointments;
+        }
 
         public static Appointment PostAppointments(Appointment userEntry)
         {
